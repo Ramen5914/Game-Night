29c4efc0793977df8f2914b7087e42e02bc78563
Omar Rahman <omar@r4men.com>
2026-05-22 00:49:35 -0700

    Changed name and updated getKnownBlocks method to match docs

--- src/main/java/com/r4men/game_night/datagen/block/GNBlockLootSubProvider.java
+++ src/main/java/com/r4men/game_night/datagen/block/GNBlockLootSubProvider.java
@@ -1,7 +1,6 @@
 package com.r4men.game_night.datagen.block;
 
 import com.r4men.game_night.block.GNBlocks;
-import net.minecraft.core.Holder;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.loot.BlockLootSubProvider;
 import net.minecraft.world.flag.FeatureFlags;
@@ -10,19 +9,22 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.Set;
 
-public class GNBlockLootTableProvider extends BlockLootSubProvider {
-    public GNBlockLootTableProvider(HolderLookup.Provider registries) {
-        super(Set.of(), FeatureFlags.REGISTRY.allFlags(), registries);
+public class GNBlockLootSubProvider extends BlockLootSubProvider {
+    public GNBlockLootSubProvider(HolderLookup.Provider lookupProvider) {
+        super(Set.of(), FeatureFlags.DEFAULT_FLAGS, lookupProvider);
     }
 
     @Override
     protected void generate() {
-        dropSelf(GNBlocks.CHESS.get());
-        dropSelf(GNBlocks.MONOPOLY.get());
+        this.dropSelf(GNBlocks.CHESS.get());
+        this.dropSelf(GNBlocks.MONOPOLY.get());
     }
 
     @Override
     protected @NotNull Iterable<Block> getKnownBlocks() {
-        return GNBlocks.BLOCKS.getEntries().stream().map(Holder::value)::iterator;
+        return GNBlocks.BLOCKS.getEntries()
+                .stream()
+                .map(e -> (Block) e.value())
+                .toList();
     }
 }
