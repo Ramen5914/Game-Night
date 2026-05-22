f5a56fc81cdd4538b52580b3182e13236d74c1d3
Omar Rahman <omar@r4men.com>
2026-05-22 00:52:27 -0700

    Update GNBlocks.java

--- src/main/java/com/r4men/game_night/block/GNBlocks.java
+++ src/main/java/com/r4men/game_night/block/GNBlocks.java
@@ -2,9 +2,8 @@ package com.r4men.game_night.block;
 
 import com.r4men.game_night.GameNight;
 import com.r4men.game_night.block.custom.ChessBlock;
-    import com.r4men.game_night.block.custom.MonopolyBlock;
+import com.r4men.game_night.block.custom.MonopolyBlock;
 import com.r4men.game_night.item.GNItems;
-import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
@@ -12,25 +11,24 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
-import java.util.function.Supplier;
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
 
 public class GNBlocks {
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(GameNight.ID);
 
-    public static final DeferredBlock<Block> CHESS = registerBlock("chess",
-            () -> new ChessBlock(BlockBehaviour.Properties.of().noOcclusion()));
+    public static final DeferredBlock<Block> CHESS = registerBlock("chess", ChessBlock::new, BlockBehaviour.Properties::noOcclusion);
 
-    public static final DeferredBlock<Block> MONOPOLY = registerBlock("monopoly",
-            () -> new MonopolyBlock(BlockBehaviour.Properties.of().noOcclusion()));
+    public static final DeferredBlock<Block> MONOPOLY = registerBlock("monopoly", MonopolyBlock::new, BlockBehaviour.Properties::noOcclusion);
 
-    private static <T extends Block> DeferredBlock<T> registerBlock(String name, Supplier<T> block) {
-        DeferredBlock<T> toReturn = BLOCKS.register(name, block);
+    private static <T extends Block> DeferredBlock<T> registerBlock(String name, Function<BlockBehaviour.Properties, T> block, UnaryOperator<BlockBehaviour.Properties> properties) {
+        DeferredBlock<T> toReturn = BLOCKS.registerBlock(name, block, properties);
         registerBlockItem(name, toReturn);
         return toReturn;
     }
 
     private static <T extends Block> void registerBlockItem(String name, DeferredBlock<T> block) {
-        GNItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+        GNItems.ITEMS.registerSimpleBlockItem(name, block, Item.Properties::useBlockDescriptionPrefix);
     }
 
     public static void register(IEventBus eventBus) {
