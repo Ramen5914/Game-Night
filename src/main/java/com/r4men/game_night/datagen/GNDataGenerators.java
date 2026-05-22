4e72943f811f0c9efec7e8f73f545d3cc4a6f87f
Omar Rahman <omar@r4men.com>
2026-05-22 00:48:56 -0700

    Changed Name

--- src/main/java/com/r4men/game_night/datagen/GNDataGenerators.java
+++ src/main/java/com/r4men/game_night/datagen/GNDataGenerators.java
@@ -1,10 +1,18 @@
 package com.r4men.game_night.datagen;
 
 import com.r4men.game_night.GameNight;
+import com.r4men.game_night.datagen.block.GNBlockLootTableSubProvider;
+import com.r4men.game_night.datagen.lang.GNEnUsLanguageProvider;
 import com.r4men.game_night.datagen.models.GNModelProvider;
+import net.minecraft.data.loot.LootTableProvider;
+import net.minecraft.data.loot.LootTableProvider.SubProviderEntry;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
+
+import java.util.List;
+import java.util.Set;
 
 @EventBusSubscriber(modid = GameNight.ID)
 public class GNDataGenerators {
@@ -12,5 +20,17 @@ public class GNDataGenerators {
     @SubscribeEvent
     public static void gatherData(GatherDataEvent.Client event) {
         event.createProvider(GNModelProvider::new);
+        event.createProvider(GNEnUsLanguageProvider::new);
+        event.createProvider((packOutput, lookupProvider) -> new LootTableProvider(
+                packOutput,
+                Set.of(),
+                List.of(
+                        new SubProviderEntry(
+                                GNBlockLootTableSubProvider::new,
+                                LootContextParamSets.BLOCK
+                        )
+                ),
+                lookupProvider
+        ));
     }
 }
