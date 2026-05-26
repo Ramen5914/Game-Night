e1ff8bcfff04a0f01cdf99976abc8bf4b7426058
Omar Rahman <omar@r4men.com>
2026-05-26 02:54:04 -0700

    Update GNDataGenerators.java

--- src/main/java/com/r4men/game_night/datagen/GNDataGenerators.java
+++ src/main/java/com/r4men/game_night/datagen/GNDataGenerators.java
@@ -1,7 +1,8 @@
 package com.r4men.game_night.datagen;
 
 import com.r4men.game_night.GameNight;
-import com.r4men.game_night.datagen.block.GNBlockLootTableSubProvider;
+import com.r4men.game_night.datagen.block.GNBlockLootSubProvider;
+import com.r4men.game_night.datagen.block.GNBlockTagsProvider;
 import com.r4men.game_night.datagen.lang.GNEnUsLanguageProvider;
 import com.r4men.game_night.datagen.models.GNModelProvider;
 import net.minecraft.data.loot.LootTableProvider;
@@ -26,11 +27,12 @@ public class GNDataGenerators {
                 Set.of(),
                 List.of(
                         new SubProviderEntry(
-                                GNBlockLootTableSubProvider::new,
+                                GNBlockLootSubProvider::new,
                                 LootContextParamSets.BLOCK
                         )
                 ),
                 lookupProvider
         ));
+        event.createProvider(GNBlockTagsProvider::new);
     }
 }
