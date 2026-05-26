55a11e96b678b14ba6cc0cba25dfaeef80c06a2f
Omar Rahman <omar@r4men.com>
2026-05-26 10:39:02 -0700

    Update GNRecipeProvider.java

--- src/main/java/com/r4men/game_night/datagen/recipe/GNRecipeProvider.java
+++ src/main/java/com/r4men/game_night/datagen/recipe/GNRecipeProvider.java
@@ -1,28 +1,29 @@
 package com.r4men.game_night.datagen.recipe;
 
-import com.r4men.game_night.GameNight;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.data.CachedOutput;
+import net.minecraft.data.DataProvider;
 import net.minecraft.data.PackOutput;
-import net.minecraft.data.recipes.RecipeProvider;
-import net.neoforged.neoforge.common.conditions.IConditionBuilder;
 import net.neoforged.neoforge.fluids.FluidType;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.concurrent.CompletableFuture;
 
-public class GNRecipeProvider extends RecipeProvider implements IConditionBuilder {
+public class GNRecipeProvider implements DataProvider {
     public static final int BUCKET = FluidType.BUCKET_VOLUME;
     public static final int BOTTLE = BUCKET / 4;
     public static final int BLOCK = 1296;
     public static final int INGOT = BLOCK / 9;
     public static final int NUGGET = INGOT / 9;
 
-    public GNRecipeProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
-        super(output, registries);
+    public GNRecipeProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {}
+
+    @Override
+    public CompletableFuture<?> run(CachedOutput cachedOutput) {
+        return CompletableFuture.completedFuture(null);
     }
 
     @Override
-    public @NotNull String getName() {
-        return GameNight.NAME + "'s Recipes";
+    public String getName() {
+        return "Game Night Recipes";
     }
 }
