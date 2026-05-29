988d4af041ce304d0e9c6cd0ada591d0892da39a
Omar Rahman <omar@r4men.com>
2026-05-29 02:14:03 -0700

    Update GNMenuTypes.java

--- src/main/java/com/r4men/game_night/gui/GNMenuTypes.java
+++ src/main/java/com/r4men/game_night/gui/GNMenuTypes.java
@@ -1,13 +1,11 @@
 package com.r4men.game_night.gui;
 
 import com.r4men.game_night.GameNight;
-import com.r4men.game_night.gui.chess.menu.ChessMenu;
+import com.r4men.game_night.gui.menu.ChessMenu;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.MenuType;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.common.extensions.IMenuTypeExtension;
-import net.neoforged.neoforge.network.IContainerFactory;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
@@ -16,11 +14,7 @@ public class GNMenuTypes {
             DeferredRegister.create(Registries.MENU, GameNight.ID);
 
     public static final DeferredHolder<MenuType<?>, MenuType<ChessMenu>> CHESS_MENU =
-            registerMenuType("chess_menu", ChessMenu::new);
-
-    private static <T extends AbstractContainerMenu> DeferredHolder<MenuType<?>, MenuType<T>> registerMenuType(String name, IContainerFactory<T> factory) {
-        return MENUS.register(name, () -> IMenuTypeExtension.create(factory));
-    }
+            MENUS.register("chess_menu", () -> IMenuTypeExtension.create(ChessMenu::new));
 
     public static void register(IEventBus eventBus) {
         MENUS.register(eventBus);
