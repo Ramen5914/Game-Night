294c4946c33973fdd59657bd580680e2447cc3cd
Omar Rahman <omar@r4men.com>
2026-05-29 02:08:44 -0700

    Updated ChessBlock to send extra data when opening menu

--- src/main/java/com/r4men/game_night/block/custom/ChessBlock.java
+++ src/main/java/com/r4men/game_night/block/custom/ChessBlock.java
@@ -2,8 +2,13 @@ package com.r4men.game_night.block.custom;
 
 import com.mojang.serialization.MapCodec;
 import com.r4men.game_night.block.entity.ChessBlockEntity;
+import com.r4men.game_night.gui.menu.ChessMenu;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.MenuProvider;
+import net.minecraft.world.SimpleMenuProvider;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
@@ -11,6 +16,7 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.jspecify.annotations.NonNull;
 
 public class ChessBlock extends SquareBoardBlock {
     public static final MapCodec<ChessBlock> CODEC = simpleCodec(ChessBlock::new);
@@ -30,13 +36,19 @@ public class ChessBlock extends SquareBoardBlock {
     }
 
     @Override
-    protected @NotNull InteractionResult useWithoutItem(@NotNull BlockState state, @NotNull Level level, @NotNull BlockPos pos, @NotNull Player player, @NotNull BlockHitResult hitResult) {
-        if (level.getBlockEntity(pos) instanceof ChessBlockEntity chessBlockEntity) {
-            if (!player.isCrouching() && !level.isClientSide()) {
-                player.openMenu(chessBlockEntity, pos);
+    protected @Nullable MenuProvider getMenuProvider(@NonNull BlockState state, @NonNull Level level, @NonNull BlockPos pos) {
+        return new SimpleMenuProvider(
+                (containerId, playerInv, player) -> new ChessMenu(containerId, playerInv, level.getBlockEntity(pos)),
+                Component.translatable("game_night.games.chess.title"));
+    }
 
-                return InteractionResult.SUCCESS;
-            }
+    @Override
+    protected @NotNull InteractionResult useWithoutItem(@NotNull BlockState state, @NotNull Level level, @NotNull BlockPos pos, @NotNull Player player, @NotNull BlockHitResult hitResult) {
+        if (!level.isClientSide() && player instanceof ServerPlayer serverPlayer) {
+            serverPlayer.openMenu(
+                    state.getMenuProvider(level, pos),
+                    buf -> buf.writeBlockPos(pos)
+            );
         }
 
         return InteractionResult.SUCCESS;
